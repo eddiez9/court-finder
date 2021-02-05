a4ec856432c3bb3e8a0e19198744cf63226ddbd9
eddiez9 <eddie@eddiez.me>
2021-02-05 22:25:06 +1100

    available courts in data structure

--- fetch.py
+++ fetch.py
@@ -3,9 +3,14 @@ import datetime
 
 site = "nbc"
 location_dict = {"silverwater":1, "sevenhills":2, "homebush":3}
-location = location_dict["sevenhills"]
-today = datetime.datetime.now()
+location = location_dict["silverwater"]
 
-yepbooking.pull(site, location, today)
+play_duration = 2
+start_datetime = datetime.datetime(2021, 2, 18, 0)
+end_datetime = datetime.datetime(2021, 2, 21, 0)
+
+nbc_avail = yepbooking.check(site, location, start_datetime)
+
+print(nbc_avail)
 
 
--- yepbooking.py
+++ yepbooking.py
@@ -2,16 +2,20 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-def pull(site, location, date):
+def check(site, location, date):
+    # Combine URL
     base_url = ".yepbooking.com.au/ajax/ajax.schema.php"
     schema_url = site + base_url 
 
     print("Fetching:" + site + " Location:" + str(location) + " " + str(date.day) + "/" + str(date.month) + "/" + str(date.year))
 
+    # Get schema response
     html_text = requests.get("https://" + schema_url, params={"day": date.day, "month": date.month, "year": date.year, "id_sport": location}).text
 
+    # Parse using html5lib leniant parsing
     soup = BeautifulSoup(html_text, "html5lib")
 
+    # Get the number of courts and their names
     lanes_table = soup.find_all("table", class_= "schemaLaneTable")
 
     lanes_list = []
@@ -20,37 +24,59 @@ def pull(site, location, date):
         for lane in lanes:
             lanes_list.append(lane.text)
 
+    # Get the actual availabilty table
     availability_table = soup.find_all("table", class_= "schemaIndividual")
 
     times_list = []
+    availability_list = {}
+
     for item in availability_table:
         rows = item.find_all("tr")
         
+        # Row 2 currently has the time slots
         times = rows[1].find_all("td")
         for time in times:
+            # Not currently used
             times_list.append(time.text)
 
-        print (times_list)
 
         lane_count = 0
-        
+        # Row 3 and after except the last row contains availability
         for x in range(2, len(rows)-1):
-            
             slots = rows[x].find_all("td", class_="hour")
-            print(lanes_list[lane_count])
-            lane_count += 1
-
+            
+            # print(lanes_list[lane_count])
+            
             for slot in slots:
                 # If time slot closed/booked/in the past
                 if "title" in slot.attrs:
-                    print(slot.attrs["title"])
+                    # We don't really care if it's closed or booked
+                    continue
+                    # print(slot.attrs["title"])
 
                 # If time slot available
                 else:
-                    print (slot.contents[0].attrs["title"])
+                    # Split by dash and grab slot start time
+                    start_time = slot.contents[0].attrs["title"].split("–")
+                    start_date = str(date.day) + "/" + str(date.month) + "/" + str(date.year) + " " + start_time[0]
+                    
+                    # Make date object
+                    date_obj = datetime.strptime(start_date, "%d/%m/%Y %I:%M%p")
 
-            print("-----")
+                    # Create dictionary of time slot -> list of available courts [Court 1, Court 2]
+                    if date_obj in availability_list.keys():
+                        availability_list[date_obj].append(lanes_list[lane_count])
+                    else:
+                        availability_list[date_obj] = [lanes_list[lane_count]]
+
+                    # print(date_obj)
+                    # print(slot.contents[0].attrs["title"])
+
+            lane_count += 1
+            # print("-----")
             
+    return(availability_list)
+    
                 
 
                 
